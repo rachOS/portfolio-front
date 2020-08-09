f07cf3a5967a9b2a5ea13c73c340437d7780c31f
rachOS <g.chamekh@e-nautia.com>
2020-08-09 15:32:35 +0200

    npm install react-slick slick-carousel Carousel is working

--- src/components/portfolio/Carousel.jsx
+++ src/components/portfolio/Carousel.jsx
@@ -1,34 +1,41 @@
 import React from "react";
-import { Link, useRouteMatch } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 // import Material UI
-import { IconButton, Box, Avatar } from "@material-ui/core";
-import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
-import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
-import AvatarGroup from "@material-ui/lab/AvatarGroup";
+import { Avatar, Container } from "@material-ui/core";
+
+// import react-click
+import Slider from "react-slick";
 
 // import style
 import "../style/portfolio.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function Carousel({ projects }) {
-    let { path, url } = useRouteMatch();
-    const avatar = projects.map((project) => (
-        <Avatar variant="square" alt={project.name} src="#">
-            <Link to={`/portfolio/${project.id}/description`}>{project.name}</Link>
-        </Avatar>
-    ));
-    console.log('URL', url);
+    const settings = {
+        dots: true,
+        dotsClass: "slick-dots slick-thumb",
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    };
 
     return (
-        <Box className="carousel">
-            <IconButton>
-                <ArrowBackIosIcon />{" "}
-            </IconButton>
-            <AvatarGroup id="avatargroup1">{avatar}</AvatarGroup>
-            <IconButton>
-                <ArrowForwardIosIcon />
-            </IconButton>
-        </Box>
+        <Container className="carousel">
+            <Slider {...settings}>
+                {projects.map((project, index) => (
+                    <Link to={`/portfolio/${project.id}/description`}>
+                        <Avatar
+                            key={index}
+                            src="src/static/image/avatar/linkedin _avatar.jpeg"
+                            alt={project.name}
+                        />
+                    </Link>
+                ))}
+            </Slider>
+        </Container>
     );
 }
 
