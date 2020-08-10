229efa574e8784d8440939aecfb96b85a3acc3ad
rachOS <g.chamekh@e-nautia.com>
2020-08-10 22:42:43 +0200

    npm install react-pdf Display pdf in Contact.jsx ok!
    ==> display fixed with : pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
    https://github.com/wojtekmaj/react-pdf/issues/321#issuecomment-451291757

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import Axios from "axios";
+import { Document, Page, pdfjs } from "react-pdf";
+
+// import Material UI
 import {
     Paper,
     Box,
@@ -7,32 +10,56 @@ import {
     CardContent,
     Button,
     CardActions,
+    Typography,
+    Container,
 } from "@material-ui/core";
-
-// import components
-
-// import data
 
 // import style
 import "../App.css";
 import "./style/details.css";
 
+import pdf from "../static/CV/Grégory_Chamekh_DéveloppeurWebFullstack-junior-v10b.pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
 function Details() {
     const [cv, setCv] = useState();
-
-    const getCV = () => {
-        const cvURL = `${process.env.REACT_APP_HOST}/infos/download`;
-        Axios.get(cvURL).then((response) => console.log("RES", response));
+    const [numPages, setNumPages] = useState(null);
+    const [pageNumber, setPageNumber] = useState(1);
+    const onDocumentLoadSuccess = ({ numPages }) => {
+        setNumPages(numPages);
     };
+    const onLoadError = (error) => {
+        return error.message;
+    };
+    const getCV = async () => {
+        const cvURL = `${process.env.REACT_APP_HOST}/infos/download`;
+        await Axios.get(cvURL)
+            .then((response) => response.data)
+            .then((data) => setCv(data));
+    };
+
+    useEffect(() => {
+        getCV();
+    }, []);
 
     return (
         <Box className="main">
             <Paper className="details-container">
                 <Card className="cv">
-                    CV
-                    <CardActions>
-                        <Button onClick={() => getCV()}>Télécharger</Button>
-                    </CardActions>
+                    <CardContent>
+                        <Document
+                            className="cv"
+                            file={pdf}
+                            onLoadSuccess={onDocumentLoadSuccess}
+                            onLoadError={onLoadError}
+                        >
+                            <Page pageNumber={1} />
+                        </Document>
+
+                        <CardActions>
+                            <Button onClick={() => getCV()}>Télécharger</Button>
+                        </CardActions>
+                    </CardContent>
                 </Card>
                 <Card className="infos">Disponibilités et infos</Card>
                 <Card className="form">Une question?</Card>
