e55aa05d042e873e45b452bbc414ebd9b562c5b2
rachOS <g.chamekh@e-nautia.com>
2020-08-18 11:17:25 +0200

    Resize CV preview in LeftContent.jsx et add download button
    <Link to={pdf} target="__blank" download>
    	<Button>Télécharger</Button>
    </Link>

--- src/components/LeftContent.jsx
+++ src/components/LeftContent.jsx
@@ -1,22 +1,27 @@
 // import core
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 // import library
-import { Document, Page, pdfjs } from "react-pdf";
+import {
+    Document,
+    Page,
+    StyleSheet,
+    View,
+    pdfjs,
+} from "react-pdf/dist/entry.webpack";
+import portrait from "../static/image/avatar/linkedin _avatar.jpeg";
 
 // import Material UI
 import {
-    Paper,
-    Avatar,
-    Box,
-    Button,
-    Card,
-    CardActions,
-    CardContent,
-    Typography,
     Accordion,
     AccordionSummary,
     AccordionDetails,
+    Avatar,
+    Box,
+    Button,
+    makeStyles,
+    Typography,
 } from "@material-ui/core";
 
 //import style
@@ -27,8 +32,30 @@ import "./style/left-menu.css";
 import pdf from "../static/CV/Grégory_Chamekh_DéveloppeurWebFullstack-junior-v10b.pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: "flex",
+        flexDirection: "column",
+        width: "auto",
+        maxWidth: "100%",
+        height: "auto",
+    },
+    accordion_container: {
+        height: "100%",
+        border: "1px solid red",
+    },
+    large: {
+        width: theme.spacing(10),
+        height: theme.spacing(10),
+    },
+    accordion_details_cv: {
+        flexWrap: "wrap",
+        justifyContent: "center",
+    },
+}));
 // init component
 const LeftContent = () => {
+    const classes = useStyles();
     const [numPages, setNumPages] = useState(null);
     const onDocumentLoadSuccess = ({ numPages }) => {
         setNumPages(numPages);
@@ -37,14 +64,15 @@ const LeftContent = () => {
         return error.message;
     };
     return (
-        <Box className="left-menu accordions">
-            <Accordion>
+        <Box className={`left-menu accordions ${classes.root}`}>
+            <Accordion className={classes.accordion_container}>
                 <AccordionSummary>
                     <Typography variant="h2">A propos</Typography>
                     <Avatar
-                        object
+                        className={classes.large}
+                        variant="rounded"
                         alt="Grégory chamekh"
-                        src="public/static/linkedin _avatar.jpeg"
+                        src={portrait}
                     />
                     <Typography variant="subtitle2">
                         Grégory Chamekh, né le 19 juin 1977 à Talence,
@@ -61,8 +89,8 @@ const LeftContent = () => {
                         Développeur Web et Web Mobile cursus ReactJS / NodeJS.
                         De part mon implication et ma participation active
                         durant cette formation, le status d'ambassadeur m'a été
-                        proposé, représentant les éléves les plus engagés dans
-                        le but de co-construire la communauté. En ce moment je
+                        proposé, représentant les éléves les plus engagés ayant
+                        pour but de co-construire la communauté. En ce moment je
                         prépare le passage du titre professionnel qui aura lieu
                         à la fin de l'année 2020, en parallèle avec le
                         développement de quelques projets personnels. Par la
@@ -75,26 +103,27 @@ const LeftContent = () => {
                     </Typography>
                 </AccordionDetails>
             </Accordion>
-            <Accordion>
+            <Accordion className={classes.accordion_container}>
                 <AccordionSummary>
                     <Typography variant="h2">C.V.</Typography>
                     <Button classes="">Afficher</Button>
                 </AccordionSummary>
-                <AccordionDetails>
+                <AccordionDetails className={classes.accordion_details_cv}>
                     <Typography variant="body2" gutterBottom>
                         <Document
-                            className="cv"
                             file={pdf}
                             onLoadSuccess={onDocumentLoadSuccess}
                             onLoadError={onLoadError}
                         >
-                            <Page pageNumber={1} />
+                            <Page pageNumber={1} height="300" />
                         </Document>
                     </Typography>
+                    <Link to={pdf} target="__blank" download>
+                        <Button>Télécharger</Button>
+                    </Link>
                 </AccordionDetails>
-                <Button>Télécharger</Button>
             </Accordion>
-            <Accordion>
+            <Accordion className={classes.accordion_container}>
                 <AccordionSummary>
                     <Typography variant="h2">Dispos</Typography>
                     <Typography variant="subtitle2">
@@ -103,11 +132,12 @@ const LeftContent = () => {
                     <Button>Plus d'infos</Button>
                 </AccordionSummary>
                 <AccordionDetails>
-                    Je suis actuellement en recherche d'un stage de fin d'études
-                    afin de pouvoir monter en compétences en vue de la
-                    préparation du passage du titre professionnel DWWM. Je suis
-                    également ouvert à une prise de poste pour un investissement
-                    et une évolution de carrière au sein de votre entreprise.
+                    Je suis actuellement en recherche de stage de fin d'études
+                    afin de pouvoir monter en compétences. Aussi, ce stage
+                    pourra me servir comme support pour du passage du titre
+                    professionnel DWWM. Je suis également ouvert à une prise de
+                    poste pour un investissement et une évolution de carrière au
+                    sein de votre entreprise.
                 </AccordionDetails>
             </Accordion>
         </Box>
